6098e7e5837539e42264d05f7ffb096a9eba5c65
Cyri0 <nemestamas94@gmail.com>
2024-03-04 17:35:57 +0100

    view created

--- links/views.py
+++ links/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import MyLink
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+# http://localhost:8000/page/admin
+# http://localhost:8000/page/teszt_elek
+
+def getPageByUser(request, username):
+    user = User.objects.get(username=username)
+    if user is not None:
+        links = MyLink.objects.filter(user = user, visible = True)
+        return render(request, 'page.html', {'links':links})
+    return render(request, 'page.html', {'msg':"Valami nem király..."})
